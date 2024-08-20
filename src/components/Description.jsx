da6db2d8720180b824d83d1c66d836af6576b2d9
Fernando Iquiza <fmiquiza@gmail.com>
2024-08-20 17:51:10 -0400

    Added initial components

---
+++ src/components/Description.jsx
@@ -0,0 +1,12 @@
+const Description = () => {
+    return (
+        <div className="description">
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+
+    );  
+};
+
+export default Description;
---
+++ src/components/logo.jsx
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src={logo} className="App-logo" alt="logo" />
+        
+        </div>
+    );
+};
+
+export default Logo;
