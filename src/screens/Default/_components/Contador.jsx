524d7ce29ed37be51d398815a78dbe1c8ffd4c50
Fernando Iquiza <fmiquiza@gmail.com>
2024-08-22 15:14:11 -0400

    adding a function to the useState to modify the value

--- src/screens/Default/_components/Contador.jsx
+++ src/screens/Default/_components/Contador.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";   
 
 const Contador = () => {
-    const [contador] = useState(10);
+    const [contador, setContador] = useState(10);
+    
+    const handleContador = () => {
+        setContador(contador + 1);
+    }
+ 
+
     return (
         <>
         <h3>Componente Contador</h3>
         <h3>{contador}</h3>
-        
+        <div>
+            <button onClick={handleContador}>Incrementar contador</button>
+        </div>
         </>
     );
 };
