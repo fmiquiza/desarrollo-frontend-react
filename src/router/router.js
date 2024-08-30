e86ff9ed0b6f4f27bbbd3c7baf1011a6e0601d55
Fernando Iquiza <fmiquiza@gmail.com>
2024-08-29 20:42:34 -0400

    Fixed issue after deployment in Github Pages

--- src/router/router.js
+++ src/router/router.js
@@ -6,7 +6,7 @@ import LoginForm from "../screens/Forms/LoginForm";
 
 import App from "../App";
 
-import LandingPage from '../../../../MODULO7/desarrollo-frontend-react/src/screens/LandingPage/LandingPage';
+import LandingPage from '../screens/LandingPage/LandingPage';
 const basename = process.env.NODE_ENV === 'production' ? '/desarrollo-frontend-react' : '/';
 
 const routes = createBrowserRouter([
